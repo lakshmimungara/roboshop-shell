9ea140971a563f46c6ec0ac9c73690b705cba9f2
lakshmimungara <lakshmimungara2997@gmail.com>
2024-09-01 07:06:45 +0530

    added mongodb

--- mongodb.sh
+++ mongodb.sh
@@ -3,7 +3,7 @@
 LOGS_FOLDER="/var/log/roboshop"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
-LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
+LOGFILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIMESTAMP.log"
 mkdir -p $LOGS_FOLDER
 
 USERID=$(id -u)
